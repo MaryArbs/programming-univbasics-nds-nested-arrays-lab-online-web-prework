be9466c33cb4aa5e94e8f66ab61b377235ed9d4e
Mary Arbachauskas <mary.arbachauskas@gmail.com>
2019-10-25 16:57:08 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -45,6 +45,10 @@ def matrix_lookup(matrix, row, column)
 end
 
 def matrix_update(matrix, row, column, new_value)
+  matrix_update=[
+    ["Don Gately", "Joelle van Dyne", "Pat M.", "Kate Gompert", "Bruce Green"],
+    ["Hal Incandenza", "Lyle", "Gerhard Schtitt",  "Mario Incandenza",  "Michael Pemulis"]
+  ]
   # Update the matrix location at row and column to have the value of new_value
   # Return the updated matrix
 end
